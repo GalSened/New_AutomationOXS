6c22af563505022537118f1f748b5e93b176696a
Gal <galsened@gmail.com>
2021-10-18 15:37:45 +0300

    ok

--- test/suite1-Login.test.js
+++ test/suite1-Login.test.js
@@ -1,61 +1,59 @@
-const xlsxFile = require('read-excel-file/node');
+
 const {Builder, By, Key, until} = require('selenium-webdriver');
 let driver = new Builder().forBrowser('chrome').build();
 var assert = require('chai').assert;
 var webdriver = require('selenium-webdriver');
+var TEST_DATA =require ("./TestData.json"); 
 
 
-// xlsxFile('./Data.xlsx').then((MyData) => {
-//    console.table(MyData);
-//});
 
 
-describe("Inner Suite 1", function(){
+describe("Inner Suite 1", () => {
     
 
     before( function(done){
 
-        driver.get("https://stg.oxs.co.il");
+        driver.get(TEST_DATA.url);
         driver.manage().window().maximize();
     done();
     });
 
-    after(function(){
+    after(() => {
 
         //driver.quit();
 
         
     });
 
-    beforeEach(function(){
+    beforeEach(() => {
 
 
     });
 
-    afterEach(function(){
+    afterEach(() => {
 
         // do something after test case execution is finished
         // no matter if there are failed cases
 
     });
 
-    it("Test-1 Logging in",async function(){
+    it("Test-1 Logging in",async () => {
 
         await driver.findElement(By.css('#view > div > header > div.loginButton > p')).click();
-        await driver.findElement(By.xpath('//*[@id="login-form"]/form/input[1]')).sendKeys('user@test.com');
-        await driver.findElement(By.xpath('//*[@id="login-form"]/form/input[2]')).sendKeys('123123');
+        await driver.findElement(By.xpath('//*[@id="login-form"]/form/input[1]')).sendKeys(TEST_DATA.username);
+        await driver.findElement(By.xpath('//*[@id="login-form"]/form/input[2]')).sendKeys(TEST_DATA.password);
         await driver.findElement(By.xpath('//*[@id="login-submit"]/input')).click();
     
     }).timeout(10000);
 
-    it("Test-2", function(){
+    it("Test-2", () => {
 
         // test Code
         // assertions
 
     });
 
-    it("Test-3", function(){
+    it("Test-3", ()=> {
 
         // test Code
         // assertions
